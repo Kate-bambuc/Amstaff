6412715da78094367fc9bea4067e0eedede2273c
Ekaterina <19katerina9416@mail.ru>
2021-04-29 10:53:06 +0700

    Standart

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const navItem = document.querySelectorAll("#nav__item");
+    const navItem = document.querySelectorAll("#nav__item"),
+        accordeonItems = document.querySelectorAll('#accordeon__item'),
+        accordeonTexts = document.querySelectorAll('#accordeon__text');
 
 
     navItem.forEach(item => {
@@ -8,5 +10,22 @@ window.addEventListener('DOMContentLoaded', () => {
             item.classList.toggle('active');
         });
     });
+
+    accordeonItems.forEach(item => {
+        item.addEventListener('click', () => {
+            //item.classList.toggle('active');
+            if(item.classList.contains('active')) {
+                item.classList.remove('active');
+            } else {
+                
+                accordeonItems.forEach(i => {
+                    i.classList.remove('active');
+                });
+
+                item.classList.add('active');
+            }
+        });
+    });
+
 
 });
