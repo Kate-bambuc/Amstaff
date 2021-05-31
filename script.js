9c285fa16a84417b455388d84d9224f63b28276d
Kate-bambuc <80806544+Kate-bambuc@users.noreply.github.com>
2021-05-31 15:38:57 +0700

    Commit31052021

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ window.addEventListener('DOMContentLoaded', () => {
         sliderLine = document.querySelector('#intro__block'),
         sliderNext = document.querySelector('#intro__btn-right'),
         sliderPrev = document.querySelector('#intro__btn-left'),
-        btns = document.querySelectorAll('#btn');
+        btns = document.querySelectorAll('#btn'),
+        navToggle = document.querySelector('#navToggle'),
+        nav = document.querySelector('#nav'),
+        articlesHistory = document.querySelectorAll('#articles__item-history'),
+        articlesLove = document.querySelectorAll('#articles__item-love'),
+        articlesBlockHistory = document.querySelector('#articles__history'),
+        articlesBlockLove = document.querySelector('#articles__love'),
+        articlesBlockTitleHistory = document.querySelector('#articles__block-title-history'),
+        articlesBlockTitleLove = document.querySelector('#articles__block-title-love');
+        
 
         let scrollPos = document.documentElement.scrollTop,
             offset = 0;	
@@ -19,6 +28,11 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Burger Menu
+    navToggle.addEventListener('click', () => {
+        navToggle.classList.toggle('show');
+        nav.classList.toggle('show');
+    });
    
      // Scroll
 
@@ -39,23 +53,27 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // ScrollBtn
-    btns.forEach(item => {
-        item.addEventListener('click', (event) => {
-            event.preventDefault();
-
-            let id = item.getAttribute('data-scroll');
-
-            document.querySelector(id).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-
-            
-            
-        });
-    });
+    function scrollFunc(i) {
+        i.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
     
+                let id = item.getAttribute('data-scroll');
+    
+                document.querySelector(id).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+    
+                
+                
+            });
+        });
+    }
+
+    // ScrollBtn
+    scrollFunc(btns);
+
 
     // Fixed menu
 
@@ -93,21 +111,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
 //    Slider Intro
 
+const mediaQuery480 = window.matchMedia('(max-width: 480px)');
+
+    function changeLength(e) {
+        if (e.matches) {
+            totalLength = 900;
+            itemLength = 300;
+        }
+    }
+
+    let totalLength = 1440,
+        itemLength = 480;
+
+    mediaQuery480.addListener(changeLength);
+    changeLength(mediaQuery480);
+
     sliderNext.addEventListener('click', () => {
-        offset += 480;
-        if (offset >= 1440) {
+        offset += itemLength;
+        if (offset >= totalLength) {
             offset = 0;
         }
         sliderLine.style.left = -offset + 'px';
     });
 
     sliderPrev.addEventListener('click', () => {
-        offset = offset - 480;
+        offset = offset - itemLength;
         if (offset < 0) {
-            offset = 960;
+            offset = totalLength - itemLength;
         }
         sliderLine.style.left = -offset + 'px';
     });
+
+
+    
 
 
     // Slider males and females
@@ -121,11 +157,12 @@ window.addEventListener('DOMContentLoaded', () => {
         malesRight = document.querySelector('#males__arrow-right'),
         femalesLeft = document.querySelector('#females__arrow-left'),
         femalesItem = document.querySelectorAll('#females__carousel-item'),
-        femalesRight = document.querySelector('#females__arrow-right'),
-        dogsPhotos = 4;
+        femalesRight = document.querySelector('#females__arrow-right');
+        
 
     let malesI = 0,
-        femalesI = 0;
+        femalesI = 0,
+        dogsPhotos = 4;
 
         
     // males
@@ -134,7 +171,7 @@ window.addEventListener('DOMContentLoaded', () => {
         
         if( malesItem[dogsPhotos + malesI]) { // Проверяет существует ли следующее фото 
             malesItem[malesI].classList.add('block');
-            malesItem[dogsPhotos].classList.remove('block');
+            malesItem[dogsPhotos + malesI].classList.remove('block');
             malesI++;
         }
     });
@@ -142,7 +179,7 @@ window.addEventListener('DOMContentLoaded', () => {
         
         if( malesItem[malesI-1]) { // Проверяет существует ли следующее фото
             malesItem[malesI-1].classList.remove('block');
-            malesItem[dogsPhotos].classList.add('block');
+            malesItem[dogsPhotos + malesI-1].classList.add('block');
             malesI--;
         }
         });
@@ -153,7 +190,7 @@ window.addEventListener('DOMContentLoaded', () => {
         
         if( femalesItem[dogsPhotos + femalesI]) { // Проверяет существует ли следующее фото 
             femalesItem[femalesI].classList.add('block');
-            femalesItem[dogsPhotos].classList.remove('block');
+            femalesItem[dogsPhotos + femalesI].classList.remove('block');
             femalesI++;
         }
     });
@@ -161,30 +198,101 @@ window.addEventListener('DOMContentLoaded', () => {
         
         if( femalesItem[femalesI-1]) { // Проверяет существует ли следующее фото
             femalesItem[femalesI-1].classList.remove('block');
-            femalesItem[dogsPhotos].classList.add('block');
+            femalesItem[dogsPhotos + femalesI-1].classList.add('block');
             femalesI--;
         }
         });
 
+        // mediaQuery
+
+    // if(mediaQuery.matches) {
+    //     malesItem[dogsPhotos-1].classList.add('block');
+    //     femalesItem[dogsPhotos-1].classList.add('block');
+    // }
+    const mediaQuery1280 = window.matchMedia('(max-width: 1280px)'),
+        mediaQuery1024 = window.matchMedia('(max-width: 1024px)'),
+        mediaQuery768 = window.matchMedia('(max-width: 768px)');
+    
+
+    function handleTabletChange(e) {
+        if (e.matches) {
+            
+            malesItem[dogsPhotos-1].classList.add('block');
+            femalesItem[dogsPhotos-1].classList.add('block');
+            dogsPhotos--;
+            
+        } 
+        // else {
+        //     malesItem[dogsPhotos+1].classList.remove('block');
+        //     femalesItem[dogsPhotos+1].classList.remove('block');
+        //     dogsPhotos++;
+        //     alert(`Увеличивается ${dogsPhotos}`);
+        // }
+
+      }
+
+    mediaQuery1280.addListener(handleTabletChange);
+    handleTabletChange(mediaQuery1280);
+
+    mediaQuery1024.addListener(handleTabletChange);
+    handleTabletChange(mediaQuery1024);
+
+    mediaQuery768.addListener(handleTabletChange);
+    handleTabletChange(mediaQuery768);
 
         // Articles Show
 
-    const articlesItem = document.querySelectorAll('#articles__item'),
-        articlesBlock = document.querySelector('#articles__block'),
-        articlesBlockTitle = document.querySelector('#articles__block-title');
+    function articleShow (articleItem, articleBlog, artocleTitle) {
 
-        articlesItem.forEach((item) => {
+        articleItem.forEach((item) => {
             item.addEventListener('click', (event) => {
                 event.preventDefault();
-                articlesBlock.classList.toggle('show');
+                articlesBlockHistory.classList.remove('show');
+                articlesBlockLove.classList.remove('show');
+
+
+                articleBlog.classList.toggle('show');
+
+                let id = item.getAttribute('data-scroll');
+    
+                document.querySelector(id).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
             });
         });
 
 
-        articlesBlockTitle.addEventListener('click', () => {
+        artocleTitle.addEventListener('click', (event) => {
             event.preventDefault();
-                articlesBlock.classList.toggle('show');
+            articleBlog.classList.toggle('show');
         });
+        
+    }
+
+
+    articleShow (articlesHistory, articlesBlockHistory, articlesBlockTitleHistory);
+    articleShow (articlesLove, articlesBlockLove, articlesBlockTitleLove);
+
+        // articlesHistory.forEach((item) => {
+        //     item.addEventListener('click', (event) => {
+        //         event.preventDefault();
+        //         articlesBlockHistory.classList.toggle('show');
+
+        //         let id = item.getAttribute('data-scroll');
+    
+        //         document.querySelector(id).scrollIntoView({
+        //             behavior: 'smooth',
+        //             block: 'start'
+        //         });
+        //     });
+        // });
+
+
+        // articlesBlockTitle.addEventListener('click', (event) => {
+        //     event.preventDefault();
+        //     articlesBlockHistory.classList.toggle('show');
+        // });
         
 
 });
